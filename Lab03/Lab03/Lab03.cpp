ee4c8216206c2c415ae83f8da34f6c169e2459e6
alafyte <xleb.xlebnyj@mail.ru>
2023-03-21 20:07:22 +0300

    Лабораторная работа №3

---
+++ Lab03/Lab03/Lab03.cpp
@@ -0,0 +1,49 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <iomanip> 
+#include "Salesman.h"
+#define N 5
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int d[N][N] = { //0     1     2     3   4        
+					{ INF,  20,   31,  INF, 10 },    //  0
+					{ 10,   INF,  25,  58,  74 },    //  1
+					{ 12,   30,   INF, 86,  59 },    //  2 
+					{ 27,   48,   40,  INF, 30 },    //  3
+					{ 83,   76,   52,  23,  INF } }; //  4  
+	int r[N];   /// результат 
+	int s = salesman(
+		N,          /// [in]  кол-во городов 
+		(int*)d,    /// [in]  массив [n*n] расстояний 
+		r           /// [out] массив [n] маршрут 0 x x x x  
+
+	);
+	cout << "\n-- Задача коммивояжера -- ";
+	cout << "\n-- количество  городов: " << N;
+	cout << "\n-- матрица расстояний : ";
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << "\n";
+		for (int j = 0; j < N; j++)
+
+			if (d[i][j] != INF) cout << setw(3) << d[i][j] << " ";
+
+			else cout << setw(3) << "INF" << " ";
+	}
+
+	cout << "\n-- оптимальный маршрут: ";
+	for (int i = 0; i < N; i++)
+		cout << r[i] + 1 << "-->";
+
+	cout << 1;
+	cout << "\n-- длина маршрута     : " << s << "\n";
+	system("pause");
+	return 0;
+
+}
